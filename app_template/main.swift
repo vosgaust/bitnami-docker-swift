812c55cbb977e9577629f811e441ade34da26a8b
Edu Moyan <edugar91@gmail.com>
2016-08-16 14:36:07 +0000

    che: Edu pull request

--- app_template/main.swift
+++ app_template/main.swift
@@ -20,6 +20,8 @@ let numberOfCards = 10
 var deck = Deck.standard52CardDeck()
 deck.shuffle()
 
+print("Hello world, I am Edu")
+
 for _ in 0..<numberOfCards {
     guard let card = deck.deal() else {
         print("No More Cards!")
